c8505680bd6c14bd93f416ad2e760081577b90d3
Ryou <81555178+Ryosuke1218@users.noreply.github.com>
2024-05-10 22:55:15 +0900

    init

---
+++ decide_lunch_menu/DataBase.py
@@ -0,0 +1,206 @@
+import sqlalchemy
+from sqlalchemy import Column, Integer, select, String, desc
+from sqlalchemy.orm import sessionmaker, declarative_base, scoped_session
+import numpy as np
+
+engine = sqlalchemy.create_engine('sqlite:///Lunch_Menu.sqlite', echo=False)   # エンジン作成
+Base = declarative_base()   # ベース作成
+Session = scoped_session(sessionmaker(autoflush=False, bind=engine))   # セッション作成
+Base.metadata.create_all(engine)
+
+
+class DataBase(Base):
+    __tablename__ = 'Lunch_Menu'  # テーブル名
+    id = Column("id", Integer, primary_key=True)    # id
+    store_name = Column("Store_Name", String)    # 店の名前
+    priority = Column("Priority", Integer)   # 優先度
+
+
+    def __init__(self, store_name=None, priority=None):
+        self.store_name = store_name
+        self.priority = priority
+
+    def add_data(self):  # データベースに追加
+        if self.get_index(self.store_name) is not None:
+            print("既に登録されています")
+            return
+        elif self.store_name is None:
+            print("店名が入力されていません")
+            return
+        new_entry = DataBase(store_name=self.store_name, priority=self.priority)
+        Session.add(new_entry)   # データベースに追加
+        Session.commit()    # データベースにコミット
+        # Session.close()
+
+    def get(self, index: int):   # データベースからデータを取得
+        action_info: list = []
+
+        # with Session() as session:
+        #     with session.begin():
+        stmt = select(DataBase).where(DataBase.id == index)
+        name = Session.execute(stmt)
+        row = name.fetchone()
+        if row is not None:
+            action_info = row[0].store_name
+            # action_info.append(row[0].priority)
+        else:
+            action_info = None
+        return action_info
+
+    def get_index(self, store_name: str):   # データベースからデータを取得
+        lunch = Session.query(DataBase).filter(DataBase.store_name == store_name).first()
+        if lunch is not None:
+            id = lunch.id
+        else:
+            id = None
+        return id
+
+    def delete_store_name(self, delete_store_name):
+        Session.query(DataBase).filter(DataBase.store_name == delete_store_name).delete()
+        Session.commit()
+        # session.close()
+
+    def get_priority(self, index: int):   # データベースからデータを取得
+        database = Session.query(DataBase).filter(DataBase.id == index).first()
+        if database is not None:
+            priority = database.priority
+        else:
+            priority = None
+        # session.close()
+        return priority
+    
+    def change_store_name(self, index: int, store_name: str):
+        Session.query(DataBase).filter(DataBase.id == index).update({DataBase.store_name: store_name})
+        Session.commit()
+        # session.close()
+
+    def get_last_id(self):
+        last_store_id = Session.query(DataBase).order_by(desc(DataBase.id)).first()
+        if last_store_id is not None:
+            last_id = last_store_id.id
+        else:
+            last_id = None
+        # session.close()
+        return last_id
+    
+    def get_all_priority(self):
+        stmt = select(DataBase.priority)
+        result = Session.execute(stmt)
+        priority_list = [row[0] for row in result]
+        print(priority_list)
+        return priority_list
+    
+    def get_all_store_name(self):
+        stmt = select(DataBase.store_name)
+        result = Session.execute(stmt)
+        store_name_list = [row[0] for row in result]
+        return store_name_list
+    
+    def change_priority(self, selected_store_name):
+        selected_index = self.get_index(selected_store_name)
+        next_priority = self.get_priority(selected_index) - 1
+        Session.query(DataBase).filter(DataBase.store_name == selected_store_name).update({DataBase.priority: next_priority})
+        Session.commit()
+
+
+class operation_menu:
+    def __init__(self):
+        pass
+        # self.display_menu()
+
+    def display_menu(self):
+        while True:
+            print("以下から選択してください")
+            print("1: 店名の追加")
+            print("2: 店名の変更")
+            print("3: 店名の削除")
+            print("4: 店名の表示")
+            print("5: ルーレット")
+            print("6: 終了")
+            print("入力:", end="")
+            selection = input()
+
+            if selection == "1":
+                self.add_store_name()
+            elif selection == "2":
+                self.change_name()
+            elif selection == "3":
+                self.delete_store()
+            elif selection == "4":
+                self.display_store_name()
+            elif selection == "5":
+                self.roulette()
+            elif selection == "6":
+                break
+
+
+    def add_store_name(self):
+        while True:
+            print("店名を入力してください")
+            store_name = input()
+            print("優先度を入力してください")
+            priority = int(input())
+            store = DataBase(store_name, priority)
+            store.add_data()
+            print("データを追加しました")
+
+            while True:
+                print("データ追加を続けますか？(y/n)")
+                answer = input()
+                if answer in ["y", "n"]:
+                    break
+                else:
+                    print("無効な入力です。'y' または 'n' を入力してください。")
+
+            if answer == "n":
+                break
+
+    def change_name(self):
+        self.display_store_name()
+        print("変更する店名のIDを入力してください")
+        store_id = input()
+        print("変更後の店名を入力してください")
+        store_name = input()
+        store = DataBase(store_name, 5)
+        store.change_store_name(store_id, store_name)
+        print("店名を変更しました")
+
+    def delete_store(self):
+        print("削除する店名のIDを入力してください")
+        self.display_store_name()
+        store_id = input()
+        store = DataBase()
+        store.delete_store_name(store_id)
+        print("店名を削除しました")
+
+    def display_store_name(self):
+        store = DataBase()
+        # print("--------------------")
+        store_name = store.get_all_store_name()
+        # for i in range(len(store_name)):
+        #     print(f"{i+1}: {store_name[i]}")
+        # print("--------------------")
+        return store_name
+
+    def roulette(self):
+        store = DataBase()
+        priority_list = store.get_all_priority()
+
+        index_list = list(range(len(priority_list)))
+        total_priority = sum(priority for priority in priority_list)
+        probabilities = [priority / total_priority for priority in priority_list]
+        selected_index = np.random.choice(index_list, p=probabilities)
+        selected_store_id = int(selected_index + 1)
+
+        selected_store = store.get(selected_store_id)
+        return selected_store
+    
+    def change_priority(self, selected_store_name):
+        store = DataBase()
+        store.change_priority(selected_store_name)
+
+
+
+
+if __name__ == "__main__":
+    menu = operation_menu()
---
+++ decide_lunch_menu/display_dearpy.py
@@ -0,0 +1,129 @@
+import dearpygui.dearpygui as dpg  # ライブラリをdpgの名前でインポート
+from DataBase import DataBase, operation_menu
+import dearpygui.demo as demo
+import os
+
+current_dir = os.path.dirname(__file__)
+parent_dir = os.path.dirname(current_dir)
+font_path = os.path.join(parent_dir, "NotoSerifJP-Black.otf")
+image_path = os.path.join(current_dir, "cake.jpeg")
+width, height, channels, texture_data = dpg.load_image(image_path)
+
+dpg.create_context()
+with dpg.texture_registry(show=False):
+    texture_id = dpg.add_static_texture(width/2, height/2, texture_data)
+
+with dpg.font_registry():
+    with dpg.font(font_path, 20, default_font=True) as default_font:
+        dpg.add_font_range_hint(dpg.mvFontRangeHint_Japanese)
+
+class display_menu:
+    def __init__(self):
+        self.operation  = operation_menu()
+        with dpg.window(label="Menu", width=500) as main_window:
+            # dpg.add_image(texture_id)
+
+            button1 = dpg.add_button(label="店名の追加", callback=self.add_store_button_call_back)
+            button2 = dpg.add_button(label="店名の変更", callback=self.change_store_name_call_back)
+            button3 = dpg.add_button(label="店名の削除", callback=self.delete_store_name_call_back)
+            button4 = dpg.add_button(label="店名の表示", callback=self.display_store_name_call_back)
+            button5 = dpg.add_button(label="ルーレット", callback=self.roulette_call_back)
+            button6 = dpg.add_button(label="終了", callback=self.exit_call_back, user_data=main_window)
+
+    def add_store_button_call_back(self):
+        with dpg.window(label="店名の追加", width=600, pos=[0, 0], height=500) as add_store_window:
+            store_name = dpg.add_input_text(label="店名を入力してください")
+            priority = dpg.add_input_text(label="優先度を入力してください")
+            add_store_button = dpg.add_button(label="追加")
+
+            if dpg.is_item_clicked(add_store_button):
+                store_name = dpg.get_value(store_name)
+                priority = dpg.get_value(priority)
+                store = DataBase(store_name, priority)
+                store.add_data()
+                print("データを追加しました")
+                self.delete_window_call_back(add_store_window)
+
+    def change_store_name_call_back(self):
+        with dpg.window(label="店名の変更", width=600, pos=[0, 0], height=500) as change_store_window:
+            all_storage_store_name = self.operation.display_store_name()
+            for i in range(len(all_storage_store_name)):
+                dpg.add_text(all_storage_store_name[i])
+
+            store_name = dpg.add_input_text(label="変更前の店名を入力してください")
+            change_store_name = dpg.add_input_text(label="変更後の店名を入力してください")
+            change_store_button = dpg.add_button(label="変更")
+
+            if dpg.is_item_clicked(change_store_button):
+                store_name = dpg.get_value(store_name)
+                change_store_name = dpg.get_value(change_store_name)
+                if store_name not in all_storage_store_name:
+                    print("入力された店名は存在しません")
+                elif store_name == change_store_name:
+                    print("変更前の店名と変更後の店名が同じです")
+                elif store_name == "" | change_store_name == "":
+                    print("店名が入力されていません")
+                else:
+                    store = DataBase(store_name, change_store_name)
+                    store.change_data()
+                    print("データを変更しました")
+                    self.delete_window_call_back(change_store_window)
+
+    def delete_store_name_call_back(self):
+        with dpg.window(label="店名の削除", width=600, pos=[0, 0], height=500) as delete_store_window:
+            all_storage_store_name = self.operation.display_store_name()
+            for i in range(len(all_storage_store_name)):
+                dpg.add_text(all_storage_store_name[i])
+            delete_store_name = dpg.add_input_text(label="削除する店名を入力してください")
+            delete_store_button = dpg.add_button(label="削除")
+
+            if dpg.is_item_clicked(delete_store_button):
+                store_name = dpg.get_value(delete_store_name)
+                if store_name not in all_storage_store_name:
+                    print("入力された店名は存在しません")
+                else:
+                    store = DataBase(store_name)
+                    store.delete_store_name()
+                    print("データを削除しました")
+                    self.delete_window_call_back(delete_store_window)
+
+    def display_store_name_call_back(self):
+        with dpg.window(label="店名の表示", width=600, pos=[0, 0], height=500) as display_store_window:
+            all_storage_store_name = self.operation.display_store_name()
+            for i in range(len(all_storage_store_name)):
+                dpg.add_text(all_storage_store_name[i])
+
+            exit_button = dpg.add_button(label="終了", callback=self.delete_window_call_back, user_data=display_store_window)
+
+    def roulette_call_back(self):
+        selected_store = self.operation.roulette()
+        with dpg.window(label="ルーレット", width=600, pos=[0, 0], height=500) as roulette_window:
+            dpg.add_text(selected_store)
+            accept_button = dpg.add_button(label="OK", callback=self.delete_roulette_window_call_back, user_data=(selected_store, roulette_window))
+            roulette_button = dpg.add_button(label="再抽選", callback=self.roulette_call_back)
+
+
+    def delete_roulette_window_call_back(self, sender, app_data, user_data):
+        self.operation.change_priority(selected_store_name=user_data[0])
+        dpg.delete_item(user_data[1])
+
+
+    def delete_window_call_back(self, sender, app_data, user_data):
+        dpg.delete_item(user_data)
+
+    def exit_call_back(self):
+        dpg.stop_dearpygui()
+
+    
+
+
+
+if __name__ == "__main__":
+    display = display_menu()
+    dpg.bind_font(default_font)
+
+    dpg.create_viewport(title="Lunch Menu", width=920, height=800)
+    dpg.setup_dearpygui()
+    dpg.show_viewport()
+    dpg.start_dearpygui()
+    dpg.destroy_context()
